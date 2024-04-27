f38e5eb67b830bb985348254e93ce72cbe285fd6
Jesse Brink <jbrink0925@gmail.com>
2024-04-26 21:26:22 -0400

    Fix constant to allow for fetching category with ID of 18 in call to /api/categories.

--- src/constants/constants.rs
+++ src/constants/constants.rs
@@ -1,4 +1,4 @@
-pub const TOTAL_CATEGORIES: i32 = 13;
+pub const TOTAL_CATEGORIES: i32 = 14;
 pub const CATEGORY_IDS: [&str; 15] = [
     "2", "3", "4", "6", "8", "9", "10", "11", "12", "13", "14",
     "15", "16", "17", "18"
